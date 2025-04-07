98c5fe686bc905995831084c2eea8c8cb2c2a816
TranNgocHuyen19 <tranngochuyenn1909@gmail.com>
2025-04-07 14:42:20 +0700

    create data table and get api to fill into table

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,60 @@
 import { useEffect, useState } from "react";
 import { OverviewItem } from "../../components/OverviewItem/OverviewItem";
-import { BadgeDollarSign, ShoppingCart, User } from "lucide-react";
+import { BadgeDollarSign, Pencil, ShoppingCart, User } from "lucide-react";
+import { Image, Space, Table, Tag } from "antd";
+
+const columns = [
+  {
+    title: "CUSTOMER NAME",
+    dataIndex: "customerName",
+    key: "customerName",
+    render: (_, { avatar, customerName }) => (
+      <div className="flex gap-2 items-center">
+        <Image src={`images/${avatar}`} />
+        <h2 className="font-bold">{customerName}</h2>
+      </div>
+    ),
+  },
+  {
+    title: "COMPANY",
+    dataIndex: "company",
+    key: "company",
+  },
+  {
+    title: "ORDER VALUE",
+    dataIndex: "orderValue",
+    key: "orderValue",
+  },
+  {
+    title: "ORDER DATE",
+    key: "orderDate",
+    dataIndex: "orderDate",
+  },
+  {
+    title: "STATUS",
+    key: "status",
+    render: (_, { status }) => (
+      <div className="text-center">
+        {status === "New" && <Tag color={"blue"}>{status}</Tag>}
+        {status === "In-progress" && <Tag color={"yellow"}>{status}</Tag>}
+        {status === "Completed" && <Tag color={"green"}>{status}</Tag>}
+      </div>
+    ),
+  },
+  {
+    key: "action",
+    render: () => (
+      <>
+        <Pencil className="text-gray-400" />
+      </>
+    ),
+  },
+];
 
 export function Dashboard() {
   const [totalCustomer, setTotalCustomer] = useState(0);
   const [totalPrice, setTotalPrice] = useState(0);
+  const [customers, setCustomers] = useState([]);
 
   useEffect(() => {
     const fetchCustomer = async () => {
@@ -13,6 +63,7 @@ export function Dashboard() {
         .then((data) => data);
       if (dataJson && dataJson.length > 0) {
         setTotalCustomer(dataJson.length);
+        setCustomers(dataJson);
       }
     };
 
@@ -32,9 +83,19 @@ export function Dashboard() {
     fetchOrder();
   }, []);
 
+  const data = customers.map((cus, index) => ({
+    key: index,
+    customerName: cus.customer_name,
+    company: cus.company,
+    orderValue: cus.order_value,
+    orderDate: cus.order_date,
+    status: cus.status,
+    avatar: cus.avatar,
+  }));
+
   return (
     <>
-      <section>
+      <section className="mb-10">
         <div className="flex gap-2">
           <img src={"images/Squares four 1.png"} alt="" />
           <h1 className="text-left font-bold">Overview</h1>
@@ -66,6 +127,22 @@ export function Dashboard() {
             icon={<User className="text-blue-400" />}
           />
         </div>
+      </section>
+      <section className="mb-8">
+        <div className="flex gap-2">
+          <img src={"images/Squares four 1.png"} alt="" />
+          <h1 className="text-left font-bold">Detailed report</h1>
+        </div>
+        <Table
+          className="my-5 "
+          columns={columns}
+          dataSource={data}
+          rowSelection={Object.assign(Object.assign({}))}
+          pagination={{ pageSize: 6 }}
+          footer={() => (
+            <div className="text-left">{totalCustomer} results</div>
+          )}
+        />
       </section>
     </>
   );
